b4972df76ac98dee3f45ef296508f9aa6e248175
Aleksey Petukhov <ikazman@yandex.ru>
2022-04-14 12:04:57 +0300

    Added instruction, wraps first pat of analysis

--- ab_testing/solver/hypo.py
+++ ab_testing/solver/hypo.py
@@ -19,16 +19,13 @@ class HypoPrioritization:
         self.scores['ICE'] = round(impact * confidence / efforts, 2)
         self.scores['RICE'] = round(reach * impact * confidence / efforts, 2)
 
-    def sorted_scores(self, col='ICE'):
-        """Выравниваем наименование колонки с текстом и текст по левому краю."""
-        self.styled_scores = self.scores.copy()
-        self.styled_scores.sort_values(by=col, ascending=False, inplace=True)
-        self.styled_scores = self.styled_scores.style.set_table_styles(
-            [dict(selector='th', props=[('text-align', 'left')])])
-        self.styled_scores = self.styled_scores.style.set_table_styles(
-            [dict(selector='th', props=[('text-align', 'left')])])
-        return self.styled_scores
-
     def get_priority(self):
         self.score()
         return self.scores
+
+    def make_pretty(self, styler, subset):
+        styler.background_gradient(axis=1,
+                                   vmin=styler.min(), vmax=styler.max(),
+                                   cmap="YlGnBu",
+                                   subset=[subset])
+        return styler
