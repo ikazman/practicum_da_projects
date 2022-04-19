2afee0fda609654c88823729d048c7e5387152d6
Aleksey Petukhov <ikazman@yandex.ru>
2022-04-19 18:14:25 +0300

    refactoring

--- ab_testing/solver/bayes_testing.py
+++ ab_testing/solver/bayes_testing.py
@@ -6,73 +6,49 @@ from .stat_tests import MannWhitneyU
 
 class BayesianTesting(MannWhitneyU):
 
-    def bayesian_tests(self, data, data_keys, sample_tile):
+    def bayesian_tests(self, data, data_samples):
+        """Подсчитываем вероятность быть лучшей."""
+        result = []
+        
+        # инициализируем тест
         test = DeltaLognormalDataTest()
-        test.add_variant_data('группа A',
-                              data[data_keys[0]])
-        test.add_variant_data('группа B',
-                              data[data_keys[1]])
-        test_result = test.probabs_of_being_best()
-        return sample_tile, test_result['группа A'], test_result['группа B']
+
+        for sample in data_samples:
+            sample_title = sample[0]
+            group_a = data[sample[1]]
+            group_b = data[sample[2]]
+
+            # добавляем варианты для тестирования
+            test.add_variant_data('A', group_a, replace=True)
+            test.add_variant_data('B', group_b, replace=True)
+
+            # получаем результат, фиксируя случайные числа
+            test_result = test.probabs_of_being_best(seed=42)
+
+            result.append((sample_title, test_result['A'], test_result['B']))
+
+        return result
 
     def result(self):
-
-        result = []
+        """Подготовим отчет о результатах тестирования."""
 
         # соберем данные для тестирования
         prepared_data = self.prepare_data_for_stat()
 
-        # проведем тест конверсии на сырых данных
+        # подготовим наборы данных
+        data_samples = [
+            ('Конверсия по сырым', 'sample_raw_a', 'sample_raw_b'),
+            ('Конверсия по очищенным', 'sample_clean_a', 'sample_clean_b'),
+            ('Средний чек по сырым', 'revenue_a', 'revenue_b'),
+            ('Средний чек по очищенным', 'clean_rev_a', 'clean_rev_b')
+        ]
 
-
-
-        raw_conversion_test = DeltaLognormalDataTest()
-        raw_conversion_test.add_variant_data('группа A',
-                                             prepared_data['sample_raw_a'])
-        raw_conversion_test.add_variant_data('группа B',
-                                             prepared_data['sample_raw_b'])
-        raw_conv_result = raw_conversion_test.probabs_of_being_best()
-        result.append(('Конверсия по сырым',
-                       raw_conv_result['группа A'],
-                       raw_conv_result['группа B']))
-
-        # проведем тест конверсии по «очищенным» данным
-        clean_conversion_test = DeltaLognormalDataTest()
-        clean_conversion_test.add_variant_data('группа A',
-                                               prepared_data['sample_clean_a'])
-        clean_conversion_test.add_variant_data('группа B',
-                                               prepared_data['sample_clean_b'])
-        clean_conv_result = clean_conversion_test.probabs_of_being_best()
-        result.append(('Конверсия по очищенным',
-                       clean_conv_result['группа A'],
-                       clean_conv_result['группа B']))
-
-        # проведем тест выручки на сырых данных
-        raw_revenue_test = DeltaLognormalDataTest()
-        raw_revenue_test.add_variant_data('группа A',
-                                          prepared_data['revenue_a'])
-        raw_revenue_test.add_variant_data('группа B',
-                                          prepared_data['revenue_b'])
-        raw_revenue_result = raw_revenue_test.probabs_of_being_best()
-        result.append(('Средний чек по сырым',
-                       raw_revenue_result['группа A'],
-                       raw_revenue_result['группа B']))
-
-        # проведем тест выручки по «очищенным» данным
-        clean_revenue_test = DeltaLognormalDataTest()
-        clean_revenue_test.add_variant_data('группа A',
-                                            prepared_data['clean_rev_a'])
-        clean_revenue_test.add_variant_data('группа B',
-                                            prepared_data['clean_rev_b'])
-        clean_revenue_test = clean_revenue_test.probabs_of_being_best()
-        result.append(('Средний чек по очищенным',
-                       clean_revenue_test['группа A'],
-                       clean_revenue_test['группа B']))
+        result = self.bayesian_tests(prepared_data, data_samples)
 
         result = pd.DataFrame(result)
         result.columns = ['Выборка', 'группа А (%)',
                           'группа В (%)']
-        result['группа А'] = round(result['группа А'] * 100, 2)
-        result['группа В'] = round(result['группа В'] * 100, 2)
+        result['группа А'] = round(result['группа А (%)'] * 100, 2)
+        result['группа В'] = round(result['группа В (%)'] * 100, 2)
 
         return result
