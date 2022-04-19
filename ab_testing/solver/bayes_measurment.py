c19716fa77e519b5f2c096218bd5fa00b7e0a4f3
Aleksey Petukhov <ikazman@yandex.ru>
2022-04-19 17:04:46 +0300

    Added bayesian_tests func

--- ab_testing/solver/bayes_measurment.py
+++ ab_testing/solver/bayes_measurment.py
@@ -2,4 +2,8 @@ from bayesian_testing.experiments import BinaryDataTest, DeltaLognormalDataTest
 from stat_tests import MannWhitneyU
 
 
+class BayesianTesting(MannWhitneyU):
 
+    def bayesian_tests(self):
+
+        result = []
