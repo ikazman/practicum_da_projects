e5cd92914020cd6802c3047d61891af33b13a170
Aleksey Petukhov <ikazman@yandex.ru>
2022-04-19 17:13:54 +0300

    Added test for conversion - on raw and clean

--- ab_testing/solver/bayes_measurment.py
+++ ab_testing/solver/bayes_measurment.py
@@ -7,3 +7,27 @@ class BayesianTesting(MannWhitneyU):
     def bayesian_tests(self):
 
         result = []
+
+        # соберем данные для тестирования
+        prepared_data = self.prepare_data_for_stat()
+
+        # проведем тест конверсии на сырых данных (Бета-распределение)
+        raw_conversion_test = BinaryDataTest()
+        raw_conversion_test.add_variant_data('группа A',
+                                             prepared_data['sample_raw_a'])
+        raw_conversion_test.add_variant_data('группа B',
+                                             prepared_data['sample_raw_b'])
+        raw_conv_result = raw_conversion_test.probabs_of_being_best()
+        result.append(('Конверсия по сырым',
+                       raw_conv_result))
+
+        # проведем тест конверсии по «очищенным» данным
+        raw_conversion_test = BinaryDataTest()
+        raw_conversion_test.add_variant_data('группа A',
+                                             prepared_data['sample_raw_a'])
+        raw_conversion_test.add_variant_data('группа B',
+                                             prepared_data['sample_raw_b'])
+        raw_conv_result = raw_conversion_test.probabs_of_being_best()
+        result.append(('Конверсия по сырым',
+                       raw_conv_result))
+        
